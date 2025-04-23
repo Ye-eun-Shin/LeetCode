17c06fbce89a9722451fbaa17cf1b453c60ebdaa
Ye-eun Shin <50901031+Ye-eun-Shin@users.noreply.github.com>
2025-04-24 04:28:48 +0900

    Time: 3 ms (8.19%), Space: 17.8 MB (72.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,26 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # 투 포인터 권법 사용
-        i = 0 # nums1 안에서 돌기
-        j = 0 # nums2 안에서 돌기
-
-        while i<m and j<n:
-            if nums1[i]>=nums2[j]:
-                nums1.insert(i, nums2[j])
-                i+=1
+        i=0
+        j=0
+        temp = []
+        while True:
+            if i>=m and j>=n:
+                break
+            if i>=m:
+                temp.append(nums2[j])
                 j+=1
-                m+=1 # 전체 요소 개수 업데이트
-            
+            elif j>=n:
+                temp.append(nums1[i])
+                i+=1
+            elif nums1[i]<=nums2[j]:
+                temp.append(nums1[i])
+                i+=1
             else:
-                i+=1
-        
-        if j<n:
-            while j<n:
-                nums1.insert(i, nums2[j])
-                i+=1
+                temp.append(nums2[j])
                 j+=1
-                m+=1
-        
-        while len(nums1)>m:
-            nums1.pop()
+        for i in range(m+n):
+            nums1[i] = temp[i]
